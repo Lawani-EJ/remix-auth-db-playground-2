6060c16d998d9aad009c4e94971beba7bde81098
Tiger Abrodi <tigerabrodi@gmail.com>
2024-02-02 18:05:40 +0100

    Add @faker-js/faker and @playwright/test dependencies

---
+++ tests/example.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from "@playwright/test";
+import { faker } from "@faker-js/faker";
+
+export function createRandomUser() {
+  return {};
+}
+
+test("has title", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
